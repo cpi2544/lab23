dfc3178032d488465c26cf9fec59b1352f48bd01
Your Name <pokpong1789@gmai.com>
2023-03-03 15:38:31 +0700

    lab23.cpp completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+#include<cmath>
+#include<cstdlib>
+using namespace std;
 
+
+int main(int argc,char *argv[]){
+
+    if(argc == 1){
+        cout << "Please input numbers to find average.";
+    }else{
+    double sum = 0;
+    
+    for(int i = 1; i<argc ; i++){
+        int a = atoi(argv[i]);
+        sum += a;
+    }
+    cout << "---------------------------------\n";
+    cout << "Average of " << argc-1 << " numbers = " << sum/(argc-1);
+    cout << "\n---------------------------------\n";
+    }
+
+    return 0;
+}
